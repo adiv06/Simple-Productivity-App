418dad93f72027c408c57bb920a465e3c3f63c9b
Adiv Padgilwar <adivp432@gmail.com>
2023-07-16 11:06:12 -0500

    Initial Commit

---
+++ FinalCitroHacksTimer/FinalCitroHacksTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalCitroHacksTimerApp.swift
+//  FinalCitroHacksTimer
+//
+//  Created by Parineeta Padgilwar on 7/16/23.
+//
+
+import SwiftUI
+
+@main
+struct FinalCitroHacksTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
